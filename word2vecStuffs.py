5a59353661627b9e4e56b20a3f8ce030864576e9
Samantha Ramsey <sramsey@andrew.cmu.edu>
2019-05-23 10:39:55 -0400

    neatened up the doc simimlarity code

--- word2vecStuffs.py
+++ word2vecStuffs.py
@@ -12,57 +12,6 @@ warnings.filterwarnings(action = 'ignore')
   
 import gensim 
 from gensim.models import Word2Vec 
-  
-all_files = ""
-files = []
-resume_cnt=0
-resumes = []
-listings = []
-
-for f in os.listdir("Resumes+listings"):
-    if(len(f.split("."))==1 and f!="Listings"):
-        sample = open("Resumes+listings/"+f, "r") 
-        text = sample.read()
-        all_files +=  text
-        files.append(text)
-        resume_cnt+=1
-        resumes.append((f,text))
-    all_files+=" "
-for f in os.listdir("Resumes+listings/Listings"):
-    if(len(f.split("."))==1):
-        sample = open("Resumes+listings/Listings/"+f, "r") 
-        text = sample.read()
-        all_files +=  text
-        files.append(text)
-        listings.append((f,text))
-    all_files+=" "
- 
-# Replaces escape character with space 
-all_files = all_files.lower()
-f = all_files.replace("\n", " ") 
-
-most_com = ["the","be","to","of","and","a","in","that","have","i","it","for","not","on","with","as","at","this","but","by"]
-for word in most_com:
-    f = f.replace(word,"")
-
-
-
-data = [] 
-  
-# iterate through each sentence in the file 
-for i in sent_tokenize(f): 
-    temp = [] 
-      
-    # tokenize the sentence into words 
-    for j in word_tokenize(i): 
-        temp.append(j.lower()) 
-  
-    data.append(temp) 
-  
-# Create CBOW model 
-model1 = gensim.models.Word2Vec(data, min_count = 1,  
-                              size = 50, window = 4,sg=1)
-#model1 = gensim.models.KeyedVectors.load_word2vec_format('GoogleNews-vectors-negative300.bin',binary=True)
 
 
 def doc_vec(word2vec_model, doc):
@@ -70,23 +19,66 @@ def doc_vec(word2vec_model, doc):
     doc = [word for word in doc if word in word2vec_model.wv.vocab]
     return np.mean(word2vec_model[doc], axis=0)
 
-centroid_vec = []
-for f in files:
-    centroid_vec.append(doc_vec(model1,f))
-#print(centroid_vec)
 
-sim_matr = cosine_similarity(np.array(centroid_vec))
+def train_model(path_to_files, path_to_save):
+    all_files = ""
+    for f in os.listdir(path_to_files):
+        if(f[0]!="."):
+            sample = open(path_to_files+f, "r") 
+            text = sample.read()
+            all_files +=  text
+        all_files+=" "
+    
+    
+    data = [] 
+    
+    # iterate through each sentence in the file 
+    for i in sent_tokenize(f): 
+        temp = [] 
+        
+        # tokenize the sentence into words 
+        for j in word_tokenize(i): 
+            temp.append(j.lower()) 
+    
+        data.append(temp) 
+    model1 = gensim.models.Word2Vec(data, min_count = 1, size = 50, window = 4,sg=1)
+    model1.save(path_to_save)
 
 
-for i in range(resume_cnt,len(files)):
-    sim_enum = list(enumerate(sim_matr[i][:resume_cnt]))
+
+
+def output_best(path_to_resumes, path_to_listing):
+    all_files = ""
+    files = []
+    resume_cnt=0
+    resumes = []
+
+    for f in os.listdir(path_to_resumes):
+        if(f[0]!="."):
+            sample = open(path_to_resumes+f, "r") 
+            text = sample.read()
+            all_files +=  text
+            files.append(text)
+            resume_cnt+=1
+            resumes.append((f,text))
+        all_files+=" "
+    listing = open(path_to_listing, "r") 
+    text = listing.read()
+    all_files +=  text
+    files.append(text)
+    model1 = gensim.models.Word2Vec.load("word2vec.model")
+
+    centroid_vec = []
+    for f in files:
+        centroid_vec.append(doc_vec(model1,f))
+
+    sim_matr = cosine_similarity(np.array(centroid_vec))
+
+
+    sim_enum = list(enumerate(sim_matr[-1][:resume_cnt]))
     sim_enum.sort(key = lambda x: -x[1])
-    #print(sim_enum)
-    print(listings[i-resume_cnt][0],":",[resumes[x[0]][0] for x in sim_enum[:3]])
+    print(path_to_listing,":",[resumes[x[0]][0] for x in sim_enum[:3]])
 
-for i in range(resume_cnt):
-    sim_enum = list(enumerate(sim_matr[i][resume_cnt:]))
-    sim_enum.sort(key = lambda x: -x[1])
-    #print(sim_enum)
-    print(resumes[i][0],":",[listings[x[0]][0] for x in sim_enum[:2]])
 
+output_best("Resumes/","Listings/Automation")
+train_model("All_files/","test.model")
